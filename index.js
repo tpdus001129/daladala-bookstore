075f8bd79745c0ee72770e1a3e419b7c15536fbc
Eunji <ckacl369@gmail.com>
2023-11-02 01:23:27 +0900

    Style: 코드 포맷 변경

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
-import { fileURLToPath } from 'url';
-import path from 'path';
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import { fileURLToPath } from "url";
+import path from "path";
 import "dotenv/config";
 
 const { PORT } = process.env;
@@ -10,13 +10,13 @@ const { PORT } = process.env;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const swaggerSpec = YAML.load(path.join(__dirname, './swagger.yaml'))
+const swaggerSpec = YAML.load(path.join(__dirname, "./swagger.yaml"));
 
 const app = express();
 
 app.use(express.static("views"));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
