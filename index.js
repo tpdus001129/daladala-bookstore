c468ff330eb22eda304dd37b2980f06827d82158
kcjfgnl9205 <kcjfgnl9205@gmail.com>
2023-11-03 00:48:43 +0900

    Feat: mongoose연결
    - mongoose연결
    - .env환경변수 설정

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ import YAML from "yamljs";
 import { fileURLToPath } from "url";
 import path from "path";
 import "dotenv/config";
+import mongoose from "mongoose";
 import router from "./router/index.js";
 
-const { PORT } = process.env;
+const { PORT, MONGODB_URI } = process.env;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,6 +15,12 @@ const __dirname = path.dirname(__filename);
 const swaggerSpec = YAML.load(path.join(__dirname, "./swagger.yaml"));
 
 const app = express();
+
+mongoose.connect(MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log("Successfully connected to MongoDB");
+});
 
 app.use(express.json());
 
