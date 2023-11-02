2d0ad929439c95045a2ec8a06ff065314ebf97d4
kcjfgnl9205 <kcjfgnl9205@gmail.com>
2023-11-02 19:34:42 +0900

    Feat: #23 인증 API 구조 작성

---
+++ controllers/authController.js
@@ -0,0 +1,30 @@
+import authService from "../services/authService.js";
+
+function requestBodyToObject(body) {
+  return {
+    email: body.email,
+    password: body.password,
+    phoneNumber: body.phoneNumber,
+  };
+}
+
+const authController = {
+  async signup(req, res) {
+    const userData = requestBodyToObject(req.body)
+    const user = await authService.signup(userData);
+    res.status(201).json({ access_token: user.access_token });
+  },
+
+  async login(req, res) {
+    const { email, password } = req.body;
+    const user = await authService.login(email, password);
+    res.status(200).json({ access_token: user.access_token });
+  },
+
+  async logout(req, res) {
+    await authService.logout();
+    res.status(200).send();
+  }
+}
+
+export default authController;
--- router/v1/index.js
+++ router/v1/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import categoryRouter from "./categoryRouter.js";
+import authRouter from "./authRouter.js";
 
 const router = express.Router();
 
 router.use("/categories", categoryRouter);
+router.use("/", authRouter);
 
 export default router;
