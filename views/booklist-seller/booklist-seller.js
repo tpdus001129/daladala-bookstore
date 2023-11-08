b39007f0a4c7d4d8cabe39de14a69ab0f8a5b4a1
[SW7] 김민희 <whgh4100@gmail.com>
2023-11-08 21:04:03 +0900

    Feat: #18 판매자 상품 목록 페이지 진행

--- views/booklist-seller/booklist-seller.js
+++ views/booklist-seller/booklist-seller.js
@@ -1,3 +1,5 @@
+import { clearCartItems, deleteCartItem, initIndexedDB } from "../indexedDB.js";
+
 const bookImg = document.querySelector("img");
 const bookTitle = document.querySelector(".title");
 const bookPrice = document.querySelector(".price");
@@ -5,27 +7,39 @@ const bookQuantity = document.querySelector(".quantity");
 const bookAuthor = document.querySelector(".author");
 const bookPublisher = document.querySelector(".publisher");
 
+const deleteAll = document.querySelector(".deleteAllBtn");
+const deleteItem = document.querySelector(".deleteBtn");
+
 const bookId = "654a254ab48028e071e58cf8";
 
-fetch(`/api/v1/books/${bookId}`)
-  .then((res) => {
+async function fetchData() {
+  try {
+    await initIndexedDB();
+    const res = await fetch(`/api/v1/books/${bookId}`);
     if (res.status === 200) {
-      return res.json();
+      const data = await res.json();
+      bookImg.alt = data.title;
+      bookTitle.innerHTML = data.title;
+      bookPrice.innerHTML = data.price.toLocaleString("ko-KR") + "원";
+      bookQuantity.innerHTML = `현재 재고: ${data.inventoryCount}권`;
+      bookAuthor.innerHTML = data.author;
+      bookPublisher.innerHTML = `/ ${data.publisher}`;
     } else if (res.status === 400) {
       throw new Error("Book not found");
     } else {
       throw new Error("Internal Server Error");
     }
-  })
-  .then((data) => {
-    console.log(data);
-    bookImg.alt = data.title;
-    bookTitle.innerHTML = data.title;
-    bookPrice.innerHTML = (data.price).toLocaleString("ko-KR") + "원";
-    bookQuantity.innerHTML = `현재 재고: ${data.inventoryCount}권`
-    bookAuthor.innerHTML = data.author;
-    bookPublisher.innerHTML = `/ ${data.publisher}`;
-  })
-  .catch((error) => {
+  } catch (error) {
     console.error("도서 정보를 불러오는 중 오류가 발생했습니다:", error);
-  });
+  }
+}
+// 호출
+fetchData();
+
+deleteAll.addEventListener("click", function () {
+  clearCartItems();
+});
+
+deleteItem.addEventListener("click", function () {
+  deleteCartItem(bookId);
+});
