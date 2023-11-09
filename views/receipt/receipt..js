47e208395b1114640ffc46a93db22daa989b45d0
Eunji <ckacl369@gmail.com>
2023-11-09 20:23:24 +0900

    Style: #17 주석 처리 변경

--- views/receipt/receipt..js
+++ views/receipt/receipt..js
@@ -1,44 +1,45 @@
 import { storageKey } from './storage';
 
-const userId = localStorage.getItem(storageKey.order_Id);
+document.addEventListener("DOMContentLoaded", function () {
+  const userId = localStorage.getItem(storageKey.userId);
 
-async function getUserData() {
-  try {
-    const res = await fetch(`/api/v1/users/${userId}/orders`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+  const tbody = document.querySelector('tbody');
+  const messageDiv = document.getElementById('order-list-message');
 
-    const jsonData = await res.json(); 
-    const tbody = document.querySelector('tbody');
-    const messageDiv = document.getElementById('order-list-message'); // "주문 내역이 없습니다." 메시지를 가리킬 요소
-
-    if (jsonData.length === 0) {
-      // 주문 내역이 없을 때
-      messageDiv.style.display = 'block'; // 메시지를 보이게 설정
-    } else {
-      // 주문 내역이 있을 때
-      messageDiv.style.display = 'none'; // 메시지를 숨기게 설정
-
-      jsonData.forEach(order => {
-        const template = document.querySelector('#order-complete');
-        const clone = document.importNode(template.content, true);
-
-        clone.querySelector('#createdAt').textContent = order.createdAt; 
-        clone.querySelector('#books').textContent = order.books; 
-        clone.querySelector('#aproductsPrice').textContent = order.aproductsPrice; 
-        clone.querySelector('#deliveryState').textContent = order.deliveryState;
-
-        tbody.appendChild(clone);
+  async function getUserData() {
+    try {
+      const res = await fetch(`/api/v1/users/${userId}/orders`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
       });
+
+      const jsonData = await res.json();
+
+      if (jsonData.length === 0) {
+        messageDiv.style.display = 'block';
+      } else {
+        messageDiv.style.display = 'none';
+
+        jsonData.forEach(order => {
+          const template = document.querySelector('#order-complete');
+          const clone = document.importNode(template.content, true);
+
+          clone.querySelector('#createdAt').textContent = order.createdAt;
+          clone.querySelector('#books').textContent = order.books;
+          clone.querySelector('#aproductsPrice').textContent = order.aproductsPrice;
+          clone.querySelector('#deliveryState').textContent = order.deliveryState;
+
+          tbody.appendChild(clone);
+        });
+      }
+
+      console.log(jsonData);
+    } catch (error) {
+      console.error('API 호출 중 오류 발생', error);
     }
-
-    console.log(jsonData);
-  } catch (error) {
-    console.error('API 호출 중 오류 발생', error);
   }
-}
 
-getUserData();
+  getUserData();
+});
--- views/storage.js
+++ views/storage.js
@@ -1,6 +1,5 @@
 export const storageKey = {
   userId: "USER_ID",
-  order_Id: "ORDER_ID"
 };
 
 export const storage = localStorage;
